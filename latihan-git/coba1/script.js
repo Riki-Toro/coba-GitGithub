aade586380f338ecdbb7b03ec5ce40902eca123e
Riki Widiantoro <77282337+Riki-Toro@users.noreply.github.com>
2021-05-15 23:01:07 +0700

    Update script.js

--- latihan-git/coba1/script.js
+++ latihan-git/coba1/script.js
@@ -1,1 +1,9 @@
 console.log('ok!');
+
+const tombol = document.querySelector('button');
+
+
+tombol.addEventListener('click', function() {
+    const p = document.querySelector('p');
+    p.innerHTML = 'Hallo, nama saya Riki Widiantoro, saya berumur 22 tahun dan saya tinggal di Ponorogo.';
+});
