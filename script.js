53661ba584ddef89356045ad4a26938644fc1c35
Riki-Toro <rikitoro22@gmail.com>
2021-05-24 21:48:22 +0700

    memberi efek pada button paragraf

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const paragraf = document.querySelector('.p');
 const list = document.querySelector('.l');
 
-console.log(paragraf);
-console.log(list);
+paragraf.addEventListener('click', function() {
+    const tagP = document.querySelector('.pp');
+    tagP.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero totam dicta ipsum possimus nisi illo earum quaerat illum blanditiis, ad dolorem deserunt! Harum itaque sint neque necessitatibus debitis optio nobis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam vero assumenda laborum mollitia odit, inventore consectetur veritatis laudantium provident necessitatibus unde quis sapiente aliquam reiciendis sed nobis architecto, dicta ipsum!';
+});
